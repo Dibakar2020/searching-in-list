cee0742bc60d8bb2fd1b429492ed2b35cf8e9d4a
Dibakar2020 <paldibakar7718@gmail.com>
2023-11-14 21:55:02 +0530

    using a for loop and the list append method to generate the powers-of-2 list L

--- power.py
+++ power.py
@@ -1,6 +1,6 @@
-L = [1, 2, 4, 8, 16, 32, 64]
+L=[2**i for i in range(7)]  #generate power of 2 upto 2^6
 X = 6
-
+print('list of powers-of-2 =',L)
 if 2 ** X in L:
     print('at index', L.index(2**X))
 else:
