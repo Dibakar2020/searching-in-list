9d3291d13e385757e1847431e624828879c30015
Dibakar2020 <paldibakar7718@gmail.com>
2023-11-14 21:25:12 +0530

    using Python’s for-else construction and list index method to eliminate the explicit list-indexing logic

--- power.py
+++ power.py
@@ -1,10 +1,8 @@
 L = [1, 2, 4, 8, 16, 32, 64]
-X = 5
-i = 0
-while i < len(L):
+X = 3
+for i in range (len(L)):
     if 2 ** X == L[i]:
         print('at index', i)
         break
-    i = i+1
 else:
-    print(X, 'not found') 
+    print(X, 'not found')
